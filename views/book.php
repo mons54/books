84e08b4786d04d394bec12988514a9b542022ac8
pierre <nanashi4781@gmail.com>
2019-11-15 15:55:42 +0100

    fix

--- views/book.php
+++ views/book.php
@@ -3,20 +3,29 @@
 <?php $author = $book['author'] ? $book['author'] : 'Inconnu'; ?>
 
 <div class="container">
-  <?php if (isset($searchResult)) { ?>
-    <div class="row">
-      <div class="col-md-12">
-        <a href="<?php echo $searchResult; ?>">
-          < Retour aux résultats</a> </div> </div> <?php } ?> <div class="row">
-            <div class="col-md-4 mt-3">
-              <img src="<?php echo $book['image']; ?>" class="card-img-top" alt="Image du livre <?php echo $book['title']; ?>" style="max-height: 500px;">
+    <?php if (isset($searchResult)) { ?>
+        <div class="row">
+            <div class="col-md-12">
+                <a href="<?php echo $searchResult; ?>">
+                    < Retour aux résultats
+                </a>
             </div>
-            <div class="col-md-8 mt-3">
-              <h1><?php echo $book['title']; ?></h1>
-              <div class="info">
+        </div>
+    <?php } ?>
+    <div class="row">
+        <div class="col-md-4 mt-3">
+          <img
+            src="<?php echo $book['image']; ?>"
+            class="card-img-top"
+            alt="Image du livre <?php echo $book['title']; ?>"
+            style="max-height: 500px;">
+        </div>
+        <div class="col-md-8 mt-3">
+            <h1><?php echo $book['title']; ?></h1>
+            <div class="info">
                 <?php echo $author; ?> (Auteur) - Paru en <?php echo $book['year']; ?>
-              </div>
-              <table class="table table-striped mt-4">
+            </div>
+            <table class="table table-striped mt-4">
                 <tbody>
                   <tr>
                     <td>Auteur</td>
@@ -40,61 +49,14 @@
                   </tr>
                   <tr>
                     <td>Wikipedia</td>
-                    <th><a href="<?php echo $book['wikipedia']; ?>">Voir</a></th>
+                    <th><a href="<?php echo $book['wikipedia']; ?>"target="_blank">Voir</a></th>
                   </tr>
                 </tbody>
-              </table>
-            </div>
-      </div>
+            </table>
+        </div>
     </div>
-<<<<<<< HEAD
-  <?php } ?>
-  <div class="row">
-    <div class="col-md-4 mt-3">
-      <img
-        src="<?php echo $book['image']; ?>"
-        class="card-img-top"
-        alt="Image du livre <?php echo $book['title']; ?>"
-        style="max-height: 500px;">
-    </div>
-    <div class="col-md-8 mt-3">
-      <h1><?php echo $book['title']; ?></h1>
-      <div class="info">
-        <?php echo $author; ?> (Auteur) - Paru en <?php echo $book['year']; ?>
-      </div>
-      <table class="table table-striped mt-4">
-        <tbody>
-          <tr>
-            <td>Auteur</td>
-            <th><?php echo $author; ?></th>
-          </tr>
-          <tr>
-            <td>Date de parution</td>
-            <th><?php echo $book['year']; ?></th>
-          </tr>
-          <tr>
-            <td>Pays</td>
-            <th><?php echo $book['country']; ?></th>
-          </tr>
-          <tr>
-            <td>Langue</td>
-            <th><?php echo $book['language']; ?></th>
-          </tr>
-          <tr>
-            <td>Pages</td>
-            <th><?php echo $book['pages']; ?></th>
-          </tr>
-          <tr>
-            <td>Wikipedia</td>
-            <th><a href="<?php echo $book['wikipedia']; ?>"target="_blank">Voir</a></th>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
-</div>
- <?php if ($booksAuthor ) { ?>
-    <div class="container">
+
+    <?php if ($booksAuthor ) { ?>
         <div class="row">
             <div class="col-md-12 mt-3">
               <nav aria-label="breadcrumb">
@@ -123,25 +85,19 @@
                 </div>
             <?php } ?>
         </div>
-<?php } else { ?>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 mt-3">
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb bg-dark">
-                            <li class="breadcrumb-item active text-white" aria-current="page">Du même auteur : pour le moment aucun ouvrage.</li>
-                        </ol>
-                        <img src="https://previews.123rf.com/images/arcady31/arcady311705/arcady31170500048/77956942-d%C3%A9sol%C3%A9-signe-vecteur.jpg"
-                        width=250px height=250px>
-                    </nav>
-                </div>
+    <?php } else { ?>
+        <div class="row">
+            <div class="col-md-12 mt-3">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb bg-dark">
+                        <li class="breadcrumb-item active text-white" aria-current="page">Du même auteur : pour le moment aucun ouvrage.</li>
+                    </ol>
+                    <img src="https://previews.123rf.com/images/arcady31/arcady311705/arcady31170500048/77956942-d%C3%A9sol%C3%A9-signe-vecteur.jpg"
+                    width=250px height=250px>
+                </nav>
             </div>
         </div>
-    </div>
-<?php } ?>
+    <?php } ?>
+</div>
 <?php $content = ob_get_clean(); ?>
 <?php require('public/index.php'); ?>
-=======
-    <?php $content = ob_get_clean(); ?>
-    <?php require('public/index.php'); ?>
->>>>>>> 753720297c81ef417ce47264483efbd5156c4433
